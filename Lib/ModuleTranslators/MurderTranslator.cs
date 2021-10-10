1344126c9eb7dfb93b2fe76c5ee3c91a50087716
tepel-chen <tepelchen501@gmail.com>
2021-10-10 09:22:51 +0900

    Adjust fontsize for murder

--- Lib/ModuleTranslators/MurderTranslator.cs
+++ Lib/ModuleTranslators/MurderTranslator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using HarmonyLib;
 using System.Reflection;
+using System.Linq;
 
 namespace TranslationService.ModuleTranslators
 {
@@ -21,7 +22,8 @@ namespace TranslationService.ModuleTranslators
         {
             MurderTranslator.translator = translator;
             var texts = module.GetComponentsInChildren<TextMesh>();
-            translator.SetTranslationToMeshes(texts, module, Magnifier.Default);
+            translator.SetTranslationToMeshes(texts.Where(text => text.text == "ACCUSE").ToArray(), module, Magnifier.Default);
+            translator.SetTranslationToMeshes(texts.Where(text => text.text != "ACCUSE").ToArray(), module, new Magnifier.VectorMagnifier(0.1f, 0.012132f));
 
         }
 
@@ -29,7 +31,7 @@ namespace TranslationService.ModuleTranslators
 
         public static void UpdateDisplayPostfix(MonoBehaviour __instance)
         {
-            if (translator != null) translator.SetTranslationToMeshes(__instance.GetValue<TextMesh[]>("Display"), __instance.GetComponent<KMBombModule>(), new Magnifier.VectorMagnifier(1f, 0.011566f));
+            if (translator != null) translator.SetTranslationToMeshes(__instance.GetValue<TextMesh[]>("Display"), __instance.GetComponent<KMBombModule>(), new Magnifier.VectorMagnifier(0.1f, 0.012132f));
         }
     }
 }
