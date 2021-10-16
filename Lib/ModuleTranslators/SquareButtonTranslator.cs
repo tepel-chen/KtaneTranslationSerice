bb1bbc04de43d6a74c912f432afe407576f0d818
noideuxcoco <noideuxcoco@gmail.com>
2021-10-16 16:22:01 +0200

    Add Square Button support

---
+++ Lib/ModuleTranslators/SquareButtonTranslator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using HarmonyLib;
+using System.Reflection;
+using System.Linq;
+using KeepCoding;
+
+namespace TranslationService.ModuleTranslators
+{
+    class SquareButtonTranslator : ModuleTranslator
+    {
+
+        private readonly MethodInfo mPressPostfix;
+        public SquareButtonTranslator(Harmony harmony)
+        {
+            if (isPatched || componentType == null) return;
+            isPatched = true;
+            mPressPostfix = SymbolExtensions.GetMethodInfo((MonoBehaviour __instance) => PressPostfix(__instance));
+            harmony.Patch(AccessTools.Method(componentType, "Press"), null, new HarmonyMethod(mPressPostfix));
+        }
+
+        private static readonly Type componentType = ReflectionHelper.FindType("AdvancedButton");
+        private static bool isPatched = false;
+        private static Magnifier displayMagnifier(string langCode) => langCode == "ja" ? new Magnifier.VectorMagnifier(0.1f, 0.025275f) : new Magnifier.VectorMagnifier(0.1f, 0.01685f);
+
+        public override void AwakeTranslation(KMBombModule module, Translator translator)
+        {
+            SquareButtonTranslator.translator = translator;
+            var text = module.GetComponentInChildren<TextMesh>();
+            translator.SetTranslationToMesh(text, module, displayMagnifier(translator.langCode));
+        }
+
+        public static Translator translator = null;
+        private static bool isPressPatched = false;
+
+        public static void PressPostfix(MonoBehaviour __instance)
+        {
+            if (!isPressPatched && translator != null)
+            {
+                isPressPatched = true;
+                var labels = __instance.GetValue<string[]>("buttonLabels");
+                for (int i = 0; i < labels.Length; ++i)
+                    labels[i] = translator.GetTranslation(labels[i], "Square Button");
+                __instance.SetValue("buttonLabels", labels);
+            }
+        }
+    }
+}
--- Lib/SupportAssign.cs
+++ Lib/SupportAssign.cs
@@ -71,7 +71,8 @@ namespace TranslationService
         public readonly static Dictionary<string, Func<Harmony, ModuleTranslator>> CustomDictionary = new Dictionary<string, Func<Harmony, ModuleTranslator>>() {
             {"Orientation Cube", harmony => new OnlyStartModuleTranslator(new OrientationCubeMagnifier()) },
             {"Adventure Game", harmony => new AdventureGameTranslator(harmony) },
-            {"Murder", harmony => new MurderTranslator(harmony) }
+            {"Murder", harmony => new MurderTranslator(harmony) },
+            {"Square Button", harmony => new SquareButtonTranslator(harmony) }
         };
     }
 }
